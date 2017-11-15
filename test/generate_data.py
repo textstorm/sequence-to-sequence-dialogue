ab1972e8d8771af30db291b3ac57f162ea018f91
textstorm <jiangzhewise@gmail.com>
2017-11-15 13:50:39 +0800

    generate_data update

--- test/generate_data.py
+++ test/generate_data.py
@@ -8,13 +8,15 @@ data_base = [ch for ch in data_base]
 def make_data():
   data = []
   reverse_data = []
+  sorted_data = []
   for i in range(10000):
-    seq_len = random.randint(1,27)
+    seq_len = random.randint(1,26)
     seq_idx = random.sample(range(len(data_base)), seq_len)
     seq = [data_base[j] for j in seq_idx]
     data.append("".join(seq))
     reverse_data.append("".join(reversed(seq)))
-  return data, reverse_data
+    sorted_data.append("".join(sorted(seq)))
+  return data, reverse_data, sorted_data
 
 def save_data(data_dir, data):
   f = open(data_dir, 'w')
@@ -24,6 +26,6 @@ def save_data(data_dir, data):
   f.close()
 
 if __name__ == "__main__":
-  data, reverse_data = make_data()
-  save_data("data.txt", zip(data, reverse_data))
-  
+  data, reverse_data, sorted_data = make_data()
+  save_data("data_reverse.txt", zip(data, reverse_data))
+  save_data("data_sort.txt", zip(data, sorted_data))
